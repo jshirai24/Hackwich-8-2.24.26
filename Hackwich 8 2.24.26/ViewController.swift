899372794ee1f06bbd4f3f54d1c941e04d515a1a
Jillian Shirai <jtshirai@hawaii.edu>
2026-02-24 12:13:51 -1000

    Completed Instructor Walkthrough Tasks

--- Hackwich 8 2.24.26/ViewController.swift
+++ Hackwich 8 2.24.26/ViewController.swift
@@ -7,7 +7,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate
+{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myFriendsArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+       
+        let text = myFriendsArray[indexPath.row]
+       
+        cell.textLabel?.text = text
+       
+        return cell
+        
+    }
+    
 
     var myFriendsArray: [String] = ["Lena", "Felisity", "Emma", "Zoe", "Sheridan"]
     
